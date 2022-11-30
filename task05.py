149472426b2c5c5932ebeac2d1327f86ac19bf8a
Diana\ Moroz <diva14@yandex.by>
2022-11-30 10:20:34 +0300

    task05 Fibonacci and Negafibonacci row

---
+++ task05.py
@@ -0,0 +1,18 @@
+# Задайте число. Составьте список чисел Фибоначчи,
+# в том числе для отрицательных индексов (Негафибоначчи).
+# Пример: для k = 8 список будет выглядеть так:
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+number = int(input("Введите число: "))
+def Fibonacci(num):
+    if num in [1, 2]:
+        return 1
+    else:
+        return Fibonacci(num-1) + Fibonacci(num-2)
+
+fib_list = [0]
+
+for e in range (1, number + 1):
+    fib_list.append(Fibonacci(e))
+    fib_list.insert(0, - Fibonacci(e))
+print(fib_list)
